b8f604a03fd91a2428cdce9bfe8f9c394aaa62a1
andrea longhi <andrea@spaghetticode.it>
2012-04-25 21:56:33 +0200

    add basic user model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :email do |n|
+    "user#{n}@example.com"
+  end
+
   factory :user do
+    email
+    password 'asdasd'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires email' do
+    FactoryGirl.build(:user, email: nil).should have_at_least(1).error_on(:email)
+  end
+
+  it 'requires password' do
+    FactoryGirl.build(:user, password: nil).should have_at_least(1).error_on(:password)
+  end
 end
