dba4c3d2b44685bfbf5019cf765eb972550bc085
andrea longhi <andrea@spaghetticode.it>
2012-04-25 22:10:08 +0200

    more authentication specs

--- spec/requests/registration_spec.rb
+++ spec/requests/registration_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe 'the registration process', :type => :request do
-
   def try_login_with(email, password, password_confirmation)
     fill_in 'Email', with: email
     fill_in 'Password', with: password
@@ -9,23 +8,33 @@ describe 'the registration process', :type => :request do
   end
 
   context 'when user insert valid data' do
-    it 'successfully register' do
+    before do
       visit new_user_registration_path
       within('.new_user') do
         try_login_with 'andrea@example.com', 'asdasd', 'asdasd'
       end
       click_button 'Sign up'
+    end
+
+    it 'successfully register' do
       page.body.should have_content 'Welcome! You have signed up successfully.'
+    end
+
+    it 'creates new user with expected email' do
+      User.find_by_email('andrea@example.com').should be_a(User)
     end
   end
 
   context 'when user insert invalid data' do
-    it 'fails registration' do
+    before do
       visit new_user_registration_path
       within('.new_user') do
         try_login_with '', 'asdasd', 'asdasd'
       end
       click_button 'Sign up'
+    end
+
+    it 'fails registration' do
       page.body.should have_content "Email can't be blank"
     end
   end
