2395bae92e9c928bbf097a7ef1c231dcefcb6ec0
andrea longhi <andrea@spaghetticode.it>
2012-04-25 21:47:28 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wimdu::Application.config.secret_token = 'ca4d48a693c17df545d3c90a5040e6ebb093a9b6cdb0a51eb8e00c1e69026ade73ca553ae1d5f494ad22812041cf180a48aa3d592d952b116e3080801669825b'
